2d906724cc31f8a9b2ee45c9bb61607d035aa099
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-09-02 22:35:39 +0600

    feat(server): add mutation endpoints

--- src/server/router/formula.ts
+++ src/server/router/formula.ts
@@ -1,12 +1,27 @@
+import { Prisma } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createRouter } from "./context";
+
+const defaultFormulaSelect = Prisma.validator<Prisma.FormulaSelect>()({
+  id: true,
+  title: true,
+  description: true,
+  definition: true,
+  createdAt: true,
+  updatedAt: true,
+  categoryId: true,
+  authorId: true,
+  authorName: true,
+  authorImage: true,
+});
 
 export const formulaRouter = createRouter()
   .query("all", {
     async resolve({ ctx }) {
       return ctx.prisma.formula.findMany({
         orderBy: { updatedAt: "desc" },
+        select: defaultFormulaSelect,
       });
     },
   })
@@ -16,7 +31,10 @@ export const formulaRouter = createRouter()
     }),
     async resolve({ ctx, input }) {
       const { id } = input;
-      const formula = await ctx.prisma.formula.findUnique({ where: { id } });
+      const formula = await ctx.prisma.formula.findUnique({
+        where: { id },
+        select: defaultFormulaSelect,
+      });
       if (!formula) {
         throw new TRPCError({
           code: "NOT_FOUND",
@@ -42,5 +60,54 @@ export const formulaRouter = createRouter()
         });
       }
       return formulas;
+    },
+  })
+  .mutation("add", {
+    input: z.object({
+      id: z.string().uuid().optional(),
+      title: z.string().min(1).max(64),
+      description: z.string().max(128).optional(),
+      definition: z.string().optional(),
+      categoryId: z.string().optional(),
+      authorId: z.string().cuid().optional(),
+      authorName: z.string().optional(),
+      authorImage: z.string().optional(),
+    }),
+    async resolve({ ctx, input }) {
+      const formula = await ctx.prisma.formula.create({
+        data: input,
+        select: defaultFormulaSelect,
+      });
+      return formula;
+    },
+  })
+  .mutation("edit", {
+    input: z.object({
+      id: z.string().uuid(),
+      data: z.object({
+        title: z.string().min(1).max(64),
+        description: z.string().max(128).optional(),
+        definition: z.string().optional(),
+        categoryId: z.string().optional(),
+      }),
+    }),
+    async resolve({ ctx, input }) {
+      const { id, data } = input;
+      const formula = await ctx.prisma.formula.update({
+        where: { id },
+        data,
+        select: defaultFormulaSelect,
+      });
+      return formula;
+    },
+  })
+  .mutation("delete", {
+    input: z.object({
+      id: z.string().uuid(),
+    }),
+    async resolve({ ctx, input }) {
+      const { id } = input;
+      await ctx.prisma.formula.delete({ where: { id } });
+      return { id };
     },
   });
