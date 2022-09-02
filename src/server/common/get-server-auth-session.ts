ea24b447a63141da91cd3d9e1c550648a47b7304
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-09-02 21:33:48 +0600

    feat(*): remove examples

--- src/server/common/get-server-auth-session.ts
+++ src/server/common/get-server-auth-session.ts
@@ -4,7 +4,6 @@ import type { GetServerSidePropsContext } from "next";
 import { unstable_getServerSession } from "next-auth";
 import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
 
-// Next API route example - /pages/api/restricted.ts
 export const getServerAuthSession = async (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
